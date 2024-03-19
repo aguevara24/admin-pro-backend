f44580d54a1f8df79e497105317fa43912f7eacb
Alfredo Guevara Fuentes <aguevara24@hotmail.com>
2024-03-19 11:07:13 -0600

    CORS y Express - Primer Commit

---
+++ index.js
@@ -0,0 +1,31 @@
+
+const express = require('express');
+
+require( 'dotenv' ).config();
+var cors = require( 'cors' );
+
+const { dbConnection } = require( './database/config' )
+
+// Crear el servidor de express
+const app = express();
+
+// Configurar CORS
+app.use( cors() );
+
+// Base de datos
+dbConnection();
+
+// pwd: nuOMkK5f24T55vrR
+// user: mean_user
+// Rutas
+app.get( '/', ( req, res ) => {
+
+    res.json({
+        ok: true,
+        msg: 'Hola mundo'
+    })
+});
+
+app.listen( process.env.PORT, () => {
+    console.log('Servidor corriendo en puerto ' + process.env.PORT );
+});
